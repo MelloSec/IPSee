b48d8f3bed440c21e8ef581f1033d074c92aa5a1
mellonaut <rleeazw@outlook.com>
2022-06-07 14:58:37 -0400

    Working finally

--- IPSee.ps1
+++ IPSee.ps1
@@ -1,9 +1,17 @@
-function Get-MyIp {
-    $myip = curl 'http://ifconfig.me/ip'
-    $ip = $myip.Content.ToString()
-}
-Get-MyIp
+# IPSee - IP Lookup Tool
+# MelloSec
+# A tool I made to check your exit node / do IP lookups when playing with malware
 
+# Checks your current IP
+function Get-MyIp {
+    Invoke-WebRequest "http://ifconfig.me/ip"
+}
+$myip = Get-MyIp
+$ip = $myip.Content.ToString()
+
+# Makes GET request the ipapi.com API to retrieve selected information about the IP address and store it in a CustomObject
+# By default this uses your public IP from above
+# You can also pass any IP address to this function and retrieve the same information.
 function Get-IPInfo {
     [CmdletBinding()]
     param (
@@ -26,11 +34,9 @@ Get-IPInfo $ip
 
 
 
-# if(!($ip = $myip){nslookup "$ip"})
-#    else
+
 # function get-nslookup
 # {
 #     nslookup $myip
 # }
 # get-nslookup
-# If local IP, use regular, llmnr only and AD DNS lookup
---
+++ README.md
@@ -0,0 +1,13 @@
+# IPSee
+
+Does IP Lookups with Geolocation data, for checking exit nodes and sketchy IPs
+
+## Make it yours
+
+---
+Generated with Plaster and the SampleModule template
+
+
+This is a sample Readme
+
+## Make it yours
