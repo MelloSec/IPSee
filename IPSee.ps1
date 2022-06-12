54f37530a53e57753e395d3cabd54e6c5b0e79da
mellonaut <rleeazw@outlook.com>
2022-06-12 11:35:12 -0400

    Update IPSee.ps1

--- IPSee.ps1
+++ IPSee.ps1
@@ -44,22 +44,22 @@
 
         [PSCustomObject]@{
 
-            Ip                  =  $IPObject.ip
-            CIDR                =  $IPObject.cidr
-            IsListed            =  $IPObject.is-listed
-            IsHijacked          =  $IPObject.is-hijacked
-            IsSpider            =  $IPObject.is-spider
-            IsTor               =  $IPObject.is-tor
-            IsProxy             =  $IPObject.is-proxy
-            IsMalware           =  $IPObject.is-malware
-            IsVpn               =  $IPObject.is-vpn
-            IsBot               =  $IPObject.is-bot
-            IsSpamBot           =  $IPObject.is-spam-bot
-            IsExploitBot        =  $IPObject.is-exploit-bot
-            ListCount           =  $IPObject.list-count
-            Blocklists          =  $IPObject.blocklists
-            LastSeen            =  $IPObject.last-seen
-            Sensors             =  $IPObject.sensors
+            Ip                  =  $IPObject."ip"
+            CIDR                =  $IPObject."cidr"
+            IsListed            =  $IPObject."is-listed"
+            IsHijacked          =  $IPObject."is-hijacked"
+            IsSpider            =  $IPObject."is-spider"
+            IsTor               =  $IPObject."is-tor"
+            IsProxy             =  $IPObject."is-proxy"
+            IsMalware           =  $IPObject."is-malware"
+            IsVpn               =  $IPObject."is-vpn"
+            IsBot               =  $IPObject."is-bot"
+            IsSpamBot           =  $IPObject."is-spam-bot"
+            IsExploitBot        =  $IPObject."is-exploit-bot"
+            ListCount           =  $IPObject."list-count"
+            Blocklists          =  $IPObject."blocklists"
+            LastSeen            =  $IPObject."last-seen"
+            Sensors             =  $IPObject."sensors"
         }
     }
     $BlockList = Check-NeutrinoBlocklist $ip $userId $apiKey | Write-Output
